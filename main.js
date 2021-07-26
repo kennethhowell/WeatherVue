43d51e86174705a64a1a1c33a768d481e252ebee
kennethhowell <howell.kenneth.a@gmail.com>
2021-07-26 11:59:24 -0500

    feat: hello world

---
+++ main.js
@@ -0,0 +1,23 @@
+const app = Vue.createApp({
+    data: function(){
+
+        return {
+            template: 
+
+            /*html*/`
+            <h1 >Hello world?</h1>
+            <h3>Wassup babu?</h3>
+            
+            `
+
+        }
+    }
+
+
+
+
+
+
+
+
+})
