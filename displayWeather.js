7cf29fbfd5d648a98e46e829b4adae4ba6bdceed
kennethhowell <howell.kenneth.a@gmail.com>
2021-07-27 12:27:24 -0500

    feat: complete 7 day forecast to view

--- displayWeather.js
+++ displayWeather.js
@@ -7,9 +7,20 @@ template:
 <h4>Current Temperature: {{ weather.data.current.temp + "&deg; F" }}</h4>
 <h4>Humidity: {{ weather.data.current.humidity + '%'}}</h4>
 <h5>Currently: {{ weather.data.current.weather[0].main }} / {{ weather.data.current.weather[0].description }} </h5>
-
+<br>
+<h1>Extended Forecast 8)</h1>
 <div v-for="(dailyWeather, index) in weather.data.daily">
-    {{ index }} : {{ new Date(dailyWeather.dt * 1000) }}
+    {{ index }} Your forecast for: {{ new Date(dailyWeather.dt * 1000).toLocaleDateString() }} is as follows -
+    <div>
+    High: {{ dailyWeather.temp.max }} // Low : {{ dailyWeather.temp.min }}
+    <br>
+    Humidity: {{ dailyWeather.humidity + "%"}} / Dew Point: {{ dailyWeather.dew_point }}
+    <br>
+    {{ dailyWeather.weather[0].main + " / " + dailyWeather.weather[0].description }}
+    <br>
+    Chance of rain: {{ (dailyWeather.pop * 100) + "%"}}    
+    </div>
+    <br>
 </div>
 
 
