2358dc7afc02bee8a5c05c8b7ab17f309aac3044
kennethhowell <howell.kenneth.a@gmail.com>
2021-07-27 11:17:51 -0500

    feat: investigating v-for render path for information coming async

--- displayWeather.js
+++ displayWeather.js
@@ -8,9 +8,7 @@ template:
 <h4>Humidity: {{ weather.data.current.humidity + '%'}}</h4>
 <h5>Currently: {{ weather.data.current.weather[0].main }} / {{ weather.data.current.weather[0].description }} </h5>
 
-<div v-for="days in weather.daily">
-{{ days.dt }}
-</div>
+
 
 `,
 
@@ -20,7 +18,7 @@ data: function(){
     }
 },
 
-created: function(){
+mounted: function(){
     axios.get("https://api.openweathermap.org/data/2.5/onecall?lat=29.51&lon=-98.64&exclude={part}&units=imperial&appid=" + openWeatherApiKey)
     .then(response => (this.weather = response))
 }
