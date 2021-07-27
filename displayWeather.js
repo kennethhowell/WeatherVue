41d519c6792f3fe08f6fba7d70f1e08390b7cecb
kennethhowell <howell.kenneth.a@gmail.com>
2021-07-27 12:02:01 -0500

    feat: date Obj math working, now formatting

--- displayWeather.js
+++ displayWeather.js
@@ -8,7 +8,10 @@ template:
 <h4>Humidity: {{ weather.data.current.humidity + '%'}}</h4>
 <h5>Currently: {{ weather.data.current.weather[0].main }} / {{ weather.data.current.weather[0].description }} </h5>
 
-<div v-for="dailyWeather in weather.data.daily">{{ dailyWeather }}</div>
+<div v-for="(dailyWeather, index) in weather.data.daily">
+    {{ index }} : {{ new Date(dailyWeather.dt * 1000) }}
+</div>
+
 
 `,
 
@@ -22,7 +25,6 @@ created: function(){
     axios.get("https://api.openweathermap.org/data/2.5/onecall?lat=29.51&lon=-98.64&exclude={part}&units=imperial&appid=" + openWeatherApiKey)
     .then(response => (this.weather = response))
 }
-
 
 
 })
